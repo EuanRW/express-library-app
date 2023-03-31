bc559620539c46c8778eb2db411fd7defcd68132
EuanRW <euanwilliams00@gmail.com>
2023-03-31 17:27:05 +0100

    Adding production plugins.

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const compression = require("compression");
+const helmet = require("helmet");
+const helmet = require("helmet");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -11,6 +14,15 @@ const catalogRouter = require("./routes/catalog"); //Import routes for "catalog"
 require('dotenv').config()
 
 var app = express();
+
+// Set up rate limiter: maximum of twenty requests per minute
+var RateLimit = require("express-rate-limit");
+var limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
+// Apply rate limiter to all requests
+app.use(limiter);
 
 // Set up mongoose connection
 const mongoose = require("mongoose");
@@ -30,6 +42,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression()); // Compress all routes
+app.use(helmet());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use("/", indexRouter);
