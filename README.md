05308b0f6b2e226a637726b12a48c9edfb202aa2
EuanRW <euanwilliams00@gmail.com>
2023-03-31 12:05:28 +0100

    Adding library routes and controllers.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # express-skeleton-app
+
+Nodemon start
+SET DEBUG=express-locallibrary-tutorial:* & npm run devstart
--- app.js
+++ app.js
@@ -4,20 +4,21 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+const catalogRouter = require("./routes/catalog"); //Import routes for "catalog" area of site
 
 var app = express();
 
 // Set up mongoose connection
-const mongoose = require("mongoose");
-mongoose.set('strictQuery', false);
-const mongoDB = "mongodb+srv://tempahtv:wTRLB4NQWJIwmS87@cluster0.ffx1qbh.mongodb.net/local_library?retryWrites=true&w=majority";
+// const mongoose = require("mongoose");
+// mongoose.set('strictQuery', false);
+// const mongoDB = process.env.MONGO_DB_KEY
 
-main().catch(err => console.log(err));
-async function main() {
-  await mongoose.connect(mongoDB);
-}
+// main().catch(err => console.log(err));
+// async function main() {
+//   await mongoose.connect(mongoDB);
+// }
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -29,8 +30,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/catalog", catalogRouter); // Add catalog routes to middleware chain.
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
